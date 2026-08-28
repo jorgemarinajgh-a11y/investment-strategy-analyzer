bbea993c7610770905309a6a0917e45b710abb82
Jorge García Hernández <jgh30@alu.ua.es>
2026-08-27 20:20:49 -0400

    Added comparison functionality

--- src/main.py
+++ src/main.py
@@ -14,10 +14,12 @@ def out_info(ticker_symbol):
     print(data["High"].max())
     print("\nMinimum in last year:")
     print(data["Low"].min())
+    return data
 
-def calculate_annual_return(ticker_symbol):
-    ticker = yf.Ticker(ticker_symbol)
-    data = ticker.history(period="1y")
+def calculate_annual_return(ticker_symbol, period, data):
+    if data is None:
+        ticker = yf.Ticker(ticker_symbol)
+        data = yf.Ticker(ticker_symbol).history(period = period)
     initial_price = data["Close"].iloc[0]
     final_price = data["Close"].iloc[-1]
     return_pct = ((final_price - initial_price) / initial_price) * 100
@@ -28,15 +30,38 @@ def compare_returns(annual_returns):
     
     return ordered
 
-returns = {}
+def simulator(money, time, stock, data):
+    gain = calculate_annual_return(stock, time, data) * money / 100
+    res = gain + money
+    return res, gain
 
+def simulate_all(stocks, money, time, data):
+    results = {}
+    for stock in stocks:
+        res, gain = simulator(money, time, stock, data[stock])
+        results[stock] = (res, gain)
+    return results
+
+returns = {}
+data = {}
 for stock in stocks:
-    out_info(stock)
-    return_pct = calculate_annual_return(stock)
+    data[stock] = out_info(stock)
+    return_pct = calculate_annual_return(stock, "1y", data[stock])
     returns[stock] = return_pct
     print(f"{stock}: Annual Return: {return_pct:.2f}%")
+    
 
 annual_returns = compare_returns(returns)
 print("\nAnnual Returns (Ordered):")
 for stock, return_pct in annual_returns:
     print(f"{stock}: {return_pct:.2f}%")
+
+
+results = simulate_all(stocks, 1000, "1y", data)
+
+print("\n$1000 Investment Simulation")
+
+for stock, (value, gain) in results.items():
+    print(f"{stock}:")
+    print(f"Current Value: ${value:.2f}")
+    print(f"Profit: ${gain:.2f}")
